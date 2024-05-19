9671a4c6a72e18a947ed9b5c6cbabb51bc5a45a8
rajgahlot1 <rajcommando1.38@gmail.com>
2024-05-19 08:07:22 +0530

    hello

--- rajsite/src/Website1/Header.jsx
+++ rajsite/src/Website1/Header.jsx
@@ -6,7 +6,6 @@ const Header = () => {
 <div className="row py-2 d-block d-md-flex  ">
 <div className="col-md-4 mx-2 col-6 text-danger border border-danger rounded-3"><h1>Raj Gahlot</h1></div>
 <div className="col row position-relative top-0 right-0">
-<div className="col my-md-2  my-3 linkData collapse d-md-block "><Link className="p-1 text-decoration-none btn btn-outline-primary rounded-pill" to="/">Home</Link></div>
 <div className="col  my-md-2 my-3 linkData collapse d-md-block "><Link  className="p-1 text-decoration-none btn btn-outline-primary rounded-pill" to="/about">About</Link></div>
 <div className="col  my-md-2 my-3 linkData collapse d-md-block "><Link  className="p-1 text-decoration-none btn btn-outline-primary rounded-pill" to="/service">Service</Link></div>
 <div className="col  my-md-2 my-3 linkData collapse d-md-block "><Link  className="p-1 btn btn-outline-primary rounded-pill text-decoration-none" to="/contact">Contact</Link></div>
